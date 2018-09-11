2ededfef43b8f0fec6c471b4e73312acbe3741ac
ericadu <erica.c.du@gmail.com>
2018-09-11 17:03:28 -0400

    Columns instead.

--- statistical_parity_generator.py
+++ statistical_parity_generator.py
@@ -1,3 +1,6 @@
+import numpy as np
+from random import random
+
 '''
 Dataset generator satisfying epsilon statistical parity. All dataset values are binary.
 A base dataset has 1 protected attribute column and an output.
@@ -6,45 +9,83 @@ INPUTS
 m       : int     [1, )   :   number of additional attributes
 n       : int     [1, )   :   number of samples
 biased  : boolean         :   unbiased or biased columns
-eps     : float   [0, 1]  :   epsilon
-p_y_a   : float   [0, 1]  :   positive outcome likelihood
-p_y_na  : float   [0, 1]  :   negative outcome likelihood
-p_a     : float   [0, 1]  :   attribute likelihood
-p       : float   [0, 1]  :   probability of outcome given unprotected attribute
+eps     : float   [0, 1]  :   | Pr[ Y^ = 1 | A = 1 ] - Pr[ Y^ = 1 | A = 0 ] | < epsilon 
+p_y_a   : float   [0, 1]  :   Pr[ Y^ = 1 | A = 1 ]
+p_a     : float   [0, 1]  :   Pr[ A = 1] attribute likelihood
+p       : float   [0, 1]  :   Pr[ Y^ = 1 | f0 = 0] prob. of outcome given unprotected attribute
 ========================
 ALGORITHM
 1. Populate protected attribute A with p_a
-2. Populate outcome y using attribute A and probabilities p_y_a, p_y_na
+2. Populate outcome y using attribute A and probability p_y_a and p_y_a' calculated from eps and p_y_A
 3. Populate f0:
-  a. If unbiased, populate with p = p_y_a - eps/2
+  a. If unbiased, populate with p.
   b. If biased, we use special case of Pr[Y = 1 | f_0 = 0] and Pr[Y = 1 | f_0 = 1]
 ========================
 OUTPUT
 dataset   : (m + 2) x n sized numpy matrix, where number of columns
 is the number of m additional attributes plus the protected attribute
-and the outcome.
+and the outcome. columns are f0, ..., f_(m-1), protected_attr, outcome.
 '''
 
-def generate_dataset(m, n, biased, eps, p_y_a, p_y_na, p_a, p):
-  dataset = np.zeros((num_samples, num_columns + 2))
-  count = 0
-  for i in range(n):
-    dataset[i, :] = generate_row(m, biased, eps, p_y_a, p_y_na, p_a, p)
-  return dataset
+def generate_dataset(m, n, biased, eps, p_y_a, p_a, p):
+  p_y_na = p_y_a - eps
+  validate_args(m, p_y_a, p_y_na, p_a, p)
 
-def generate_row(m, biased, eps, p_y_a, p_y_na, p_a, p):
-  protected_attribute = 1 if random() < p_a else 0
+  def get_outcome(x):
+    if (x == 1 and random() < p_y_a) or (x == 0 and random() < p_y_na):
+      return 1
+    else:
+      return 0
 
-  outcome_if_a = protected_attribute == 1 and random() < p_y_a
-  outcome_if_na = protected_attribute == 0 and random () < p_y_na
+  def get_attr(y):
+    p_y_eq_1 = p_a * p_y_a + (1 - p_a) * p_y_na
+    p_attr_0 = (p_y_eq_1 - 1 + p) / (2 * p - 1)
 
-  outcome = 1 if outcome_if_a or outcome_if_na else 0
+    p_attr_0_given_y_eq_1 = (p * p_attr) / p_y_eq_1
+    p_attr_0_given_y_eq_0 = (1 - p) * p_attr / (1 - p_y_eq_1)
 
-  if biased:
-    pos_outcome_likelihood = p_a * p_y_a + (1 - p_a) * p_y_na
-    attribute_absent = (pos_outcome_likelihood - 1 + p) / (2 * p - 1)
-    attributes = [0 if random() < attribute_absent else 1 for _ in range(m)]
-  else:
-    attributes = [1 if random() < p else 0 for _ in range(m)]
+    attr_if_1 = y == 1 and (random() < p_attr_0_given_y_eq_1)
+    attr_if_0 = y == 0 and (random() < p_attr_0_given_y_eq_0)
 
-  return [outcome, attribute] + attributes
+    if attr_if_1 or attr_if_0:
+      return 0
+    else:
+      return 1
+
+  v_outcome_func = np.vectorize(get_outcome)
+  v_attr_func = np.vectorize(get_attr)
+
+  # Step 1: Populate protected
+  protected_attr = np.array([1 if random() < p_a else 0 for _ in range(n)])
+
+  # Step 2: Populate outcome
+  outcome = v_outcome_func(protected_attr)
+
+  dataset = np.stack(protected_attr, outcome)
+
+  columns = np.zeros((m, n))
+  # Step 3: Populate columns
+  for i in range(m):
+    if biased:
+      columns[i,:] = v_attr_func(outcome))
+    else:
+      columns[i,:]= [1 if random() < p else 0 for _ in range(m)]))
+
+  return np.concatenate((columns, dataset))
+
+
+def validate_args(m, p_y_a, p_y_na, p_a, p):
+  if m < 1:
+    raise ValueError("m must be greater than 0.")
+
+  if not 0.0 <= p_y_a <= 1.0:
+    raise ValueError("p_y_a must be between 0.0 and 1.0. Currently equal to: ".format(str(p_y_a)))
+
+  if not 0.0 <= p_y_na <= 1.0:
+    raise ValueError("p_y_na must be between 0.0 and 1.0. Currently equal to: ".format(str(p_y_na)))
+
+  if not 0.0 <= p_a <= 1.0:
+    raise ValueError("p_a must be between 0.0 and 1.0. Currently equal to: ".format(str(p_a)))
+
+  if not 0.0 <= p < 1.0:
+    raise ValueError("p must be between 0.0 and 1.0. Currently equal to: ".format(str(p)))
